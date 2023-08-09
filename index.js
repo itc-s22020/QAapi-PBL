5f2c8d60b7213d61fff6e63b8ef655b0e2005f9a
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-08-09 11:29:15 +0900

    /api/icons/<user>.png でユーザーのアイコンを取得できるようになった

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require("path");
 
 const apiRouter = require('./api')
 
 app.use(express.json())
 app.use('/api', apiRouter)
+
+// "/api/icons" では/static/icons内のファイルを静的に返す（ユーザーのアイコン用）
+app.use("/api/icons", express.static(path.join("static", "icons")))
 
 const port = process.env.PORT ?? 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
