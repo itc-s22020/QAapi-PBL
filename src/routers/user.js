09e9d521a8798ab15b1ff088895435a3253b4e6d
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-08-12 15:17:05 +0900

    管理者権限を確認できるミドルウェアを実装

--- src/routers/user.js
+++ src/routers/user.js
@@ -64,8 +64,30 @@ const Auth = (req, res, next) => {
     }
 }
 
+/**
+ * 管理者権限が必要なエンドポイントに挟むミドルウェア
+ * これ以降の処理では req.user でログイン中のユーザー情報を参照できる
+ */
+
+const AuthAdmin = (req, res, next) => Auth(req, res, async () => {
+    const user = await prisma.user.findUnique({
+        where: {
+            user_id: req.user
+        }
+    }).then(r => r)
+    if (user.admin === true) {
+        next()
+    } else {
+        res.status(401).json({message: '管理者権限が必要です', user: req.user})
+    }
+})
+
 router.get('/check', Auth, (req, res) => {
     res.status(200).json({message: 'ログインしています', user: req.user})
 })
 
-module.exports = {router, Auth}
+router.get('/checkadmin', AuthAdmin, (req, res) => {
+    res.status(200).json({message: '管理者としてログインしています', user: req.user})
+})
+
+module.exports = {router, Auth, AuthAdmin}
