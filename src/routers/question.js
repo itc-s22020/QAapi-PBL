6f622a0bef211bce4e5d5b49e1deecfc9e1ba6ff
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-08-13 14:17:03 +0900

    質問一覧表示（仮）

--- src/routers/question.js
+++ src/routers/question.js
@@ -5,6 +5,41 @@ const prisma = new PrismaClient()
 
 const {Auth} = require('./user')
 
+router.get('', async (req, res) => {
+    await prisma.question.findMany({
+        include: {
+            user: true,
+            category: true,
+            answers: true,
+            best_a: true,
+        }
+    }).then((r) => {
+        res.json(r.map((q) => ({
+            q_id: q.q_id,
+            user_id: q.user_id,
+            user_name: q.user.name,
+            title: q.title,
+            q_text: q.q_text,
+            date: q.date,
+            like: q.like,
+            view: q.view,
+            c_id: q.c_id,
+            c_name: q.category.c_name,
+            best_a_id: q.best_a_id,
+            best_a: q.best_a,
+            answers: q.answers.map(a => ({
+                a_id: a.a_id,
+                user_id: a.user_id,
+                user_name: a.user.name,
+                a_text: a.a_text,
+                date: a.date,
+                like: a.like,
+                view: a.view
+            }))
+        })))
+    })
+})
+
 router.post('/new', Auth, async (req, res) => {
     const {c_id, q_text, title} = req.body
     await prisma.question.create({
