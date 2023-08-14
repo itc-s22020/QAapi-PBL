fbc7f0d4224fcd43a12d8fc7b822387ff386a069
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-08-14 21:51:48 +0900

    データベース接続、失敗のメッセージ追加

--- src/index.js
+++ src/index.js
@@ -8,3 +8,15 @@ app.use('/api', apiRouter)
 
 const port = process.env.PORT ?? 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
+
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+prisma.$connect()
+    .then(() => {
+        console.log('データベース接続OK')
+        prisma.$disconnect()
+    })
+    .catch((e) => {
+        console.log(`データベース接続失敗: ${e}`)
+        process.exit(1)
+    })
